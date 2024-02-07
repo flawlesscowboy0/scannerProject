1a545336997c021a40883a502a74744e825bc955
Nick Tanner <flawlesscowboy@gmail.com>
2024-02-07 18:48:43 -0500

    Initial prototype scanner.

---
+++ .venv/scanner.py
@@ -0,0 +1,126 @@
+"""
+Logical Steps:
+
+    1. Read entire file into string.
+    2. Split string into words (tokens) and prepare for processing. NOTE: Need to determine fate/handling of special
+    characters like \n amd \t.
+    3. Once list of words is prepared, compare against token dictionary and update attributes (identifier, keyword, etc)
+    NOTE: Probably useful to simply convert string objects in list to list objects in list (which themselves hold
+    strings) for easy handling of attributes.
+    4. Once all tokens are properly tagged, produce JSON file from list.
+    5. Ensure all output is correctly displayed and JSON file is produced correctly.
+
+    Reading the entire file into a list allows us to iterate over the list and easily handle comments both at the end of
+    a line and as a block. (See prototype logic below for an idea of how block comments can be handled via this method.
+    For end of line comments the implementation will be to check for \n after them which would imply the end of the
+    comment.)
+
+    Currently, need to implement a dictionary or devise a regex capable of parsing for the types of characters we are
+    interested in parsing. Need to implement comparison against dictionary.
+"""
+
+import json
+import pprint
+import re
+
+from pathlib import Path
+path = input("Enter path of file you wish to scan: ")
+text = Path(path).read_text()  # Reads entire file into text object.
+wordList = re.split(r"([A-Za-z]+|[0-9]+|\t+|\n+|[/*,.=]+|[\":*/]+)", text)  # This regex captures all tokens properly.
+"""
+Unfortunately, the regex above captures tokens *too* well, and we end up with a variety of weird whitespace in the list.
+The lines below simply remove the remaining empty strings so that pprint will not display them and they will not be
+errantly entered into the JSON or as an "unknown" token.
+
+Considered solutions:
+
+''.join().split() - This can remove errant leftover whitespace, but you also lose \n and \t.
+Could look into filter(), list comprehension, or lambda.
+
+The iterating whitespace eater below is as elegant as it gets for now.
+"""
+count = ""
+while len(count) < 32:  # Arbitrary length, could be easily adjusted to capture even more whitespace.
+    while count in wordList:
+        wordList.remove(count)
+    count += " "  # Append one more space to each iteration of the loop.
+pprint.pprint(wordList)  # This will pretty print the list. If split along whitespace, \n and \t will be shown.
+
+# New logic to read line by line, tokenizing as we go.
+
+# with open(path) as inputfile:
+#     while textline := inputfile.readline():
+#         words = [ ''.join(textline.split()) ]
+#         wordlist = list
+#         wordlist.append(words)
+#     print(wordlist)
+
+"""Prototyping the logic to parse the list.
+
+commentBlock updates only on hitting block comment notation. If true, no tokens should be scanned.
+
+Checks also for tab character (there is at least one in the file) and simply continues from there"""
+from tokens import *
+commentBlock = False
+inlineComment = False
+masterTokenList = []
+for word in wordList:
+    if word == "\t":  # We don't need to tag this one I don't think.
+        continue
+    elif word == "/*":
+        commentBlock = True
+    elif word == "*/":
+        commentBlock = False
+        continue
+    elif word == "//":
+        inlineComment = True
+    elif word == "\n":
+        inlineComment = False
+    # Probably also need special handling for unknown tokens and for items inside of quotations.
+    # Begin comparison logic.
+    if not commentBlock and not inlineComment:
+        # Logic to compare list elements against token dictionary should go here.
+        if word in tokenList["Keywords"]:
+            newToken = Token('Keywords', tokenList["Keywords"][word], word)
+        elif word in tokenList["Identifiers"]:
+            newToken = Token('Identifiers', tokenList["Identifiers"][word], word)
+        elif word in tokenList["Operators"]:
+            newToken = Token('Operators', tokenList["Operators"][word], word)
+        elif word in tokenList["Special Symbols"]:
+            newToken = Token('Special Symbols', tokenList["Special Symbols"][word], word)
+        else:
+            newToken = Token('Unknown Token', 7777, word)
+
+        print("New Token Created: ", newToken.getData())  # Print that token was made to console, along with data.
+        masterTokenList.append(newToken)  # Add token to master list.
+    # Example File has a new token made here, "End of Statement" but we capture \n so not sure if necessary.
+
+# Create JSON file
+
+JSONoutput = open("OutputTokens.json", "w")  # Open file for writing.
+masterDictionary = {}  # Create empty dictionary.
+counter = 0
+for Token in masterTokenList:
+    tokenID = "Token " + counter.__str__()  # Generate token ID using counter.
+    masterDictionary.update({tokenID: {}})
+    counter += 1
+
+# Helper method to convert list to dictionary.
+def createDictionary(a):
+    dictObj = iter(a)
+    return dict(zip(dictObj, dictObj))
+    # Zip takes the iterable objects out of the passed in list in pairs, creating
+    # a new dictionary object out of them.
+
+counter = 0  # Reset the clock
+for Token in masterTokenList:
+    tokenData = Token.getData()
+    dataList = ['Type', tokenData[0], 'ID', tokenData[1], 'Value', tokenData[2]]
+    tokenDataDictionary = createDictionary(dataList)
+
+    tokenID = "Token " + counter.__str__()  # Make ID same as before.
+    masterDictionary[tokenID].update(tokenDataDictionary)
+    counter += 1
+
+JSONdata= json.dumps(masterDictionary, indent=4)
+JSONoutput.write(JSONdata)  # Create data object and write to file.
---
+++ .venv/tokens.py
@@ -0,0 +1,91 @@
+# Tokens
+tokenList = {
+    "Keywords": {
+        'import': 0,
+        'implementations': 1,
+        'function': 2,
+        'main': 3,
+        'return': 4,
+        'type': 5,
+        'integer': 6,
+        'double': 7,
+        'char': 8,
+        'num': 9,
+        'is': 10,
+        'variables': 11,
+        'define': 12,
+        'of': 13,
+        'begin': 14,
+        'display': 15,
+        'set': 16,
+        'exit': 17,
+        'endfun': 18,
+        'symbol': 19,
+        'end': 20,
+        'input': 21,
+        'structures': 22,
+        'pointer': 23,
+        'head': 24,
+        'last': 25,
+        'NULL': 26,
+        'ChNode': 27,
+        'using': 28,
+        'reverse': 29,
+        'while': 30,
+        'endwhile': 31,
+        'call': 32,
+        'constants': 33,
+        'float': 34,
+        'array': 35,
+        'for': 36,
+        'to': 37,
+        'do': 38,
+        'endfor': 39
+    },
+
+    "Identifiers": {
+        'x': 100,
+        'r': 101,
+        'area': 102,
+        'cir': 103,
+        'pchar': 104,
+        'j': 105,
+        'N': 106,
+        'sum': 107,
+        'ave': 108,
+        'svalue': 109,
+        'num': 110,
+    },
+
+    "Operators": {
+        '+': 300,
+        '-': 301,
+        '*': 302,
+        '/': 303,
+        '^': 304,
+        '>': 305,
+        '<': 306,
+        '"': 407,
+        '=': 408,
+        'add': 409
+    },
+
+    "Special Symbols": {
+        '.': 500,
+        ',': 501,
+        'PI': 502,
+        'M_PI': 503,
+        '[N]': 504,
+        '\n' : 505,
+        '\t' : 506
+    }
+}
+
+class Token:
+    def __init__(self, type, id, value):
+        self.type = type
+        self.id = id
+        self.value = value
+
+    def getData(self):
+        return [self.type, self.id, self.value]
